d2aceca81bbec8107235c6f5d6409f87e610edd8
Alejandro Capilla Sanchez <alejandrocapillasanchez@MacBook-Pro-de-Alejandro.local>
2021-10-29 21:22:57 -0500

    Interfaz terminada

--- src/main/java/com/example/practica1_eq6/Main.java
+++ src/main/java/com/example/practica1_eq6/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
@@ -16,7 +17,9 @@ public class Main extends Application {
     private VBox vBox;
     private Scene escena;
     private Button btnIniciar, btnSalir;
-    private GridPane gridPane;
+    private HBox contenedorVasos;
+    private VBox vaso1, vaso2, vaso3;
+    private Label lblVaso1sup, lblVaso1inf, lblVaso2sup, lblVaso2inf, lblVaso3sup, lblVaso3inf;
 
     public static void main(String[] args) {
         launch(args);
@@ -33,15 +36,59 @@ public class Main extends Application {
     private void crearUI() {
         borderPane = new BorderPane();
         vBox = new VBox();
-        gridPane = new GridPane();
+        contenedorVasos = new HBox();
+        vaso1 = new VBox();
+        vaso2 = new VBox();
+        vaso3 = new VBox();
         btnIniciar = new Button("");
         btnSalir = new Button("");
+        lblVaso1sup = new Label();
+        lblVaso2sup = new Label();
+        lblVaso3sup = new Label();
+        lblVaso1inf = new Label();
+        lblVaso2inf = new Label();
+        lblVaso3inf = new Label();
+
+        lblVaso1inf.setPrefSize(80,40);
+        lblVaso1sup.setPrefSize(80,40);
+
+        lblVaso2inf.setPrefSize(80,40);
+        lblVaso2sup.setPrefSize(80,40);
+
+        lblVaso3inf.setPrefSize(80,40);
+        lblVaso3sup.setPrefSize(80,40);
+
+        vaso1.getChildren().addAll(lblVaso1sup,lblVaso1inf);
+        vaso2.getChildren().addAll(lblVaso2sup,lblVaso2inf);
+        vaso3.getChildren().addAll(lblVaso3sup,lblVaso3inf);
 
         btnIniciar.setPrefSize(90,65);
         btnSalir.setPrefSize(90,65);
-        gridPane.setPrefSize(250,180);
+        contenedorVasos.setPrefSize(300,180);
+        contenedorVasos.setId("panelPadre");
+        contenedorVasos.setPadding(new Insets(30));
+        contenedorVasos.setSpacing(20);
+
+        vaso1.setPrefSize(100,60);
+        vaso1.setId("vaso");
+        vaso1.setPadding(new Insets(10));
+        vaso1.setSpacing(15);
+
+        vaso2.setPrefSize(100,60);
+        vaso2.setId("vaso");
+        vaso2.setPadding(new Insets(10));
+        vaso2.setSpacing(15);
+
+        vaso3.setPrefSize(100,60);
+        vaso3.setId("vaso");
+        vaso3.setPadding(new Insets(10));
+        vaso3.setSpacing(15);
+
+        contenedorVasos.getChildren().addAll(vaso1, vaso2, vaso3);
+
         vBox.setPadding(new Insets(20));
-        vBox.getChildren().addAll(gridPane,borderPane);
+        vBox.setSpacing(20);
+        vBox.getChildren().addAll(contenedorVasos,borderPane);
         borderPane.setLeft(btnIniciar);
         borderPane.setRight(btnSalir);
 
@@ -52,11 +99,13 @@ public class Main extends Application {
         agregarImagenSalir();
 
         escena = new Scene(vBox, 400, 300);
+        escena.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
 
     }
 
     private void llenadoColores() {
         System.out.println("Se ejecuta el metodo correspondiente");
+        btnIniciar.setDisable(true);
     }
 
     private void agregarImagenIniciar() {
