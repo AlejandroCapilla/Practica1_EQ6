17a978fafdfb66e2fbfe353e2f31531a200f12f2
AlejandroCapilla <19031028@itcelaya.edu.mx>
2021-11-17 21:11:56 -0600

    Construccion interfaz

--- src/main/java/com/example/practica1_eq6/Main.java
+++ src/main/java/com/example/practica1_eq6/Main.java
@@ -16,10 +16,11 @@ public class Main extends Application {
     private BorderPane borderPane;
     private VBox vBox;
     private Scene escena;
-    private Button btnIniciar, btnSalir;
-    private HBox contenedorVasos;
+    private Button btnGenerarColor, btnSalir;
+    private HBox contenedorVasos, contenedorVasos10ml;
     private VBox vaso1, vaso2, vaso3;
-    private Label lblVaso1sup, lblVaso1inf, lblVaso2sup, lblVaso2inf, lblVaso3sup, lblVaso3inf;
+    private Label lblLiquido1, lblLiquido2, lblLiquido3;
+    private Label separadorVasos;
 
     public static void main(String[] args) {
         launch(args);
@@ -36,82 +37,82 @@ public class Main extends Application {
     private void crearUI() {
         borderPane = new BorderPane();
         vBox = new VBox();
-        contenedorVasos = new HBox();
+        contenedorVasos = new HBox(); // Contenedor donde se muestran todos los vasos
+        contenedorVasos10ml = new HBox(); // Contenedor donde se muestran los vasos de 10ml (mas chicos)
         vaso1 = new VBox();
         vaso2 = new VBox();
         vaso3 = new VBox();
-        btnIniciar = new Button("");
+        separadorVasos = new Label(""); // label que sirve para generar un espacio entre los vasos de 10ml y el de 100
+        btnGenerarColor = new Button("");
         btnSalir = new Button("");
-        lblVaso1sup = new Label();
-        lblVaso2sup = new Label();
-        lblVaso3sup = new Label();
-        lblVaso1inf = new Label();
-        lblVaso2inf = new Label();
-        lblVaso3inf = new Label();
+        // Label que muestra el contenido de cada vaso
+        lblLiquido1 = new Label();
+        lblLiquido2 = new Label();
+        lblLiquido3 = new Label();
 
-        lblVaso1inf.setPrefSize(80,40);
-        lblVaso1sup.setPrefSize(80,40);
+        lblLiquido1.setPrefSize(80,40);
+        lblLiquido2.setPrefSize(80,40);
 
-        lblVaso2inf.setPrefSize(80,40);
-        lblVaso2sup.setPrefSize(80,40);
+        vaso1.getChildren().add(lblLiquido1);
+        vaso2.getChildren().add(lblLiquido2);
 
-        lblVaso3inf.setPrefSize(80,40);
-        lblVaso3sup.setPrefSize(80,40);
+        btnGenerarColor.setPrefSize(90,40);
+        btnSalir.setPrefSize(90,40);
 
-        vaso1.getChildren().addAll(lblVaso1sup,lblVaso1inf);
-        vaso2.getChildren().addAll(lblVaso2sup,lblVaso2inf);
-        vaso3.getChildren().addAll(lblVaso3sup,lblVaso3inf);
+        contenedorVasos10ml.setPadding(new Insets(35, 0, 0, 0));
+        contenedorVasos10ml.setSpacing(30);
 
-        btnIniciar.setPrefSize(90,65);
-        btnSalir.setPrefSize(90,65);
         contenedorVasos.setPrefSize(300,180);
         contenedorVasos.setId("panelPadre");
-        contenedorVasos.setPadding(new Insets(30));
+        contenedorVasos.setPadding(new Insets(30, 30, 20, 30));
         contenedorVasos.setSpacing(20);
 
-        vaso1.setPrefSize(100,60);
+        vaso1.setPrefSize(60,40);
         vaso1.setId("vaso");
         vaso1.setPadding(new Insets(10));
         vaso1.setSpacing(15);
 
-        vaso2.setPrefSize(100,60);
+        vaso2.setPrefSize(60,40);
         vaso2.setId("vaso");
         vaso2.setPadding(new Insets(10));
         vaso2.setSpacing(15);
 
-        vaso3.setPrefSize(100,60);
+        vaso3.setPrefSize(80,60);
         vaso3.setId("vaso");
         vaso3.setPadding(new Insets(10));
         vaso3.setSpacing(15);
 
-        contenedorVasos.getChildren().addAll(vaso1, vaso2, vaso3);
+        separadorVasos.setPrefSize(40,1);
+
+        contenedorVasos10ml.getChildren().addAll(vaso1,vaso2);
+        contenedorVasos.getChildren().addAll(contenedorVasos10ml, separadorVasos, vaso3);
 
         vBox.setPadding(new Insets(20));
         vBox.setSpacing(20);
         vBox.getChildren().addAll(contenedorVasos,borderPane);
-        borderPane.setLeft(btnIniciar);
+
+        borderPane.setPadding(new Insets(0, 20, 0, 20));
+        borderPane.setLeft(btnGenerarColor);
         borderPane.setRight(btnSalir);
 
-        btnIniciar.setOnAction(event -> llenadoColores());
+        btnGenerarColor.setOnAction(event -> llenadoColores());
         btnSalir.setOnAction(event -> {System.exit(0);});
 
         agregarImagenIniciar();
         agregarImagenSalir();
 
-        escena = new Scene(vBox, 400, 300);
+        escena = new Scene(vBox, 400, 270);
         escena.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
-
     }
 
     private void llenadoColores() {
-        System.out.println("Se ejecuta el metodo correspondiente");
-        btnIniciar.setDisable(true);
+        btnGenerarColor.setDisable(true);
     }
 
     private void agregarImagenIniciar() {
         BackgroundImage myBI= null;
         try {
-            myBI = new BackgroundImage(new Image(Main.class.getResource("iniciar.png").toURI().toString(),320,320,false,true),
+            myBI = new BackgroundImage(new Image(Main.class.getResource("generarColor.png").toURI().toString(),320,320,false,true),
                     BackgroundRepeat.NO_REPEAT,
                     BackgroundRepeat.NO_REPEAT,
                     BackgroundPosition.CENTER,
@@ -120,7 +121,7 @@ public class Main extends Application {
             e.printStackTrace();
         }
 
-        btnIniciar.setBackground(new Background(myBI));
+        btnGenerarColor.setBackground(new Background(myBI));
     }
 
     private void agregarImagenSalir() {
